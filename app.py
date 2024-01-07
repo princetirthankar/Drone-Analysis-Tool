8ff8081830c81588690fd822464dbfa5c1899307
taksh <taksh335@gmail.com>
2024-01-07 13:42:23 +0530

    Added CSV routes and support to run DROP

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
-from flask import Flask
+from flask import Flask,send_file
 from flask_googlemaps import GoogleMaps
 from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
+import os
+import csv
 
 app = Flask(__name__)
 
@@ -19,6 +21,7 @@ GoogleMaps(app)
 
 @app.route('/home')
 def home():
+    os.system('python ../DROP/DROP.py  -o static/outputCSV/tmp.csv ../DROP/sample-data/DAT-Files/FLY000.DAT')
     return render_template("Home.html")
 
 
@@ -52,6 +55,29 @@ def mapview():
     )
     print("Map END POIT : ", mymap ,sndmap)
     return render_template('Map.html', mymap=mymap, sndmap=sndmap)
+
+
+@app.route('/csvroute')
+def csv_route():
+    os.system('python ../DROP/DROP.py  -o static/outputCSV/tmp.csv ../DROP/sample-data/DAT-Files/FLY000.DAT')
+    f = open("static/outputCSV/tmp.csv",'rb')
+    return send_file(f,as_attachment=True,download_name="abcd.csv")
+
+@app.route('/csvroute1')
+def csv_route1():
+    os.system('python ../DROP/DROP.py  -o static/outputCSV/tmp.csv ../DROP/sample-data/DAT-Files/FLY000.DAT')
+    f = open("static/outputCSV/tmp.csv")
+    readdata = csv.reader(f)
+    headers = next(readdata)
+    data = [headers]
+    cnt=0
+    for row in readdata:
+        if cnt>10:
+            break
+        cnt+=1
+        data.append(row)
+    print(len(data))
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
